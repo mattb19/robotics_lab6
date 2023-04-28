b866cf0b2ba061c270402d53fd61e37b885a0ee6
mattb19 <matthew51672@gmail.com>
2023-04-27 23:38:08 -0400

    Update simple_planner.py

--- scripts/simple_planner.py
+++ scripts/simple_planner.py
@@ -14,6 +14,7 @@ import numpy as np
 import tf2_ros
 from tf.transformations import *
 import tf2_geometry_msgs
+from std_msgs.msg import UInt8
 
 
 sphere_params = SphereParams()
@@ -86,6 +87,7 @@ if __name__ == '__main__':
 	
 		# move above ball
 		plan = Plan()
+        point_mode = UInt8()
 		plan_point0 = Twist()
 		plan_point0.linear.x = -0.0166
 		plan_point0.linear.y = -0.406
@@ -93,57 +95,100 @@ if __name__ == '__main__':
 		plan_point0.angular.x = 3.126
 		plan_point0.angular.y = 0.016
 		plan_point0.angular.z = 1.530
+        point_mode.data = 0
 		plan.points.append(plan_point0)
+        plan.modes.append(point_mode)
 		
 		# pick up ball
 		plan_point2 = Twist()
+        point_mode = UInt8()
 		plan_point2.linear.x = x
 		plan_point2.linear.y = y
 		plan_point2.linear.z = z
 		plan_point2.angular.x = 3.11
 		plan_point2.angular.y = 0.056
 		plan_point2.angular.z = 1.53
+        point_mode.data = 0
 		plan.points.append(plan_point2)
+        plan.modes.append(point_mode)
+        
+		# pick up ball
+		plan_point2 = Twist()
+        point_mode = UInt8()
+		plan_point2.linear.x = x
+		plan_point2.linear.y = y
+		plan_point2.linear.z = z
+		plan_point2.angular.x = 3.11
+		plan_point2.angular.y = 0.056
+		plan_point2.angular.z = 1.53
+        point_mode.data = 2
+		plan.points.append(plan_point2)
+        plan.modes.append(point_mode)
 		
 		# move the robot back above the ball
 		plan_point11 = Twist()
+        point_mode = UInt8()
 		plan_point11.linear.x = -0.0166
 		plan_point11.linear.y = -0.406
 		plan_point11.linear.z = 0.429
 		plan_point11.angular.x = 3.126
 		plan_point11.angular.y = 0.016
 		plan_point11.angular.z = 1.530
+        point_mode.data = 0
 		plan.points.append(plan_point11)
+        plan.modes.append(point_mode)
 		
 		# move the robot holding the ball to a new location
 		plan_point3 = Twist()
+        point_mode = UInt8()
 		plan_point3.linear.x = 0.233
 		plan_point3.linear.y = -0.33
 		plan_point3.linear.z = 0.429
 		plan_point3.angular.x = 3.126
 		plan_point3.angular.y = 0.016
 		plan_point3.angular.z = 1.530
+        point_mode.data = 0
 		plan.points.append(plan_point3)
+        plan.modes.append(point_mode)
 	
 		# put the ball down in new location 
 		plan_point4 = Twist()
+        point_mode = UInt8()
 		plan_point4.linear.x = 0.233
 		plan_point4.linear.y = -0.335
 		plan_point4.linear.z = 0.053
 		plan_point4.angular.x = 3.11
 		plan_point4.angular.y = 0.056
 		plan_point4.angular.z = 2.181
+        point_mode.data = 0
 		plan.points.append(plan_point4)
+        plan.modes.append(point_mode)
+        
+		# put the ball down in new location 
+		plan_point4 = Twist()
+        point_mode = UInt8()
+		plan_point4.linear.x = 0.233
+		plan_point4.linear.y = -0.335
+		plan_point4.linear.z = 0.053
+		plan_point4.angular.x = 3.11
+		plan_point4.angular.y = 0.056
+		plan_point4.angular.z = 2.181
+        point_mode.data = 1
+		plan.points.append(plan_point4)
+        plan.modes.append(point_mode)
 		
 		# pick arm back up and return to start
 		plan_point31 = Twist()
+        point_mode = UInt8()
 		plan_point31.linear.x = 0.233
 		plan_point31.linear.y = -0.33
 		plan_point31.linear.z = 0.429
 		plan_point31.angular.x = 3.126
 		plan_point31.angular.y = 0.016
 		plan_point31.angular.z = 1.530
+        point_mode.data = 0
 		plan.points.append(plan_point31)
+        plan.modes.append(point_mode)
 		
 		
 		
